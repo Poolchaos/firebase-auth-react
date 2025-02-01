1031c0dfbcf648a5c587a4e6cbb4530df83bb497
Phillip-Juan van der Berg <phillip-juan@zailab.com>
2025-02-01 10:43:49 +0200

    chore: setup husky, jest and structure

---
+++ src/tests/jest.setup.ts
@@ -0,0 +1,22 @@
+global.fetch = jest.fn(
+  () =>
+    Promise.resolve({
+      ok: true,
+      status: 200,
+      headers: { get: jest.fn() },
+    }) as unknown as Promise<Response>,
+);
+
+const mockImportMeta = {
+  env: {
+    VITE_WEATHERBIT_API_URL: "mocked-url",
+    VITE_WEATHERBIT_API_KEY: "mocked-api-key",
+    VITE_SENTRY_URL: "mocked-sentry-url",
+  },
+};
+
+Object.defineProperty(global, "import", {
+  get() {
+    return { meta: mockImportMeta };
+  },
+});
---
+++ src/tests/mock.test.ts
@@ -0,0 +1,7 @@
+import { expect } from "@jest/globals";
+
+describe("mock test", () => {
+  it("tests whether the initial tests work", async () => {
+    expect(true).toBe(true);
+  });
+});
